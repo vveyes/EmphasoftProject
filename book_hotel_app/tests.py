f5dee31221b1ee2717e557a318cf65f55fb5b8ad
vveyes <strezhnev7@mail.ru>
2024-04-02 14:49:08 -0400

    prefinal commit

--- book_hotel_app/tests.py
+++ book_hotel_app/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='tester2048',
+            email='will@email.com',
+            first_name = 'will',
+            last_name='willer',
+            password='12345',
+        )
+        self.assertEqual(user.username, 'tester2048')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertEqual(user.first_name, 'will')
+        self.assertEqual(user.last_name, 'willer')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username='superuser',
+            email='admin@email.com',
+            first_name = 'admin',
+            last_name='adminer',
+            password='12345',
+        )
+        self.assertEqual(user.username, 'superuser')
+        self.assertEqual(user.email, 'admin@email.com')
+        self.assertEqual(user.first_name, 'admin')
+        self.assertEqual(user.last_name, 'adminer')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c@_@kw^j!r%k%81m1)sn=-%3!$m_fg^*19x9%l$bs9s-15-sja'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,11 +96,11 @@ WSGI_APPLICATION = 'hotel_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hotelbase',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
